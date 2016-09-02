95e3bbffa08ca9d7d4150a7f658d91af6bc8bace
jleewebdev <lee45276@gmail.com>
2016-09-02 18:46:53 -0400

    Mailgun production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,14 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.default_url_options = {:host => "yingwenxuexiao.com"}
-
+config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yingwenxuexiao.com', #eg: 'yourappname.herokuapp.com'
+    :authentication => :plain,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
