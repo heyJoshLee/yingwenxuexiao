9443bc00ff5570a70e955ea441941205a02912cc
jleewebdev <lee45276@gmail.com>
2016-11-02 22:18:56 +0800

    Protect word_search routes

--- app/controllers/games/word_search_controller.rb
+++ app/controllers/games/word_search_controller.rb
@@ -3,10 +3,6 @@ class Games::WordSearchController < ApplicationController
   before_action :require_paid_membership
 
 
-  def index
-  end
-
-
   def found_word
     current_user.add_points(2)
     respond_to do |format|
