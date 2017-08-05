c724be744183e1a1229a72749905409e0509b6f4
Josh Lee <lee45276@gmail.com>
2017-08-05 21:05:13 +0800

    Add report pages in dashboard for payments and users.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,8 @@
 class Payment < ActiveRecord::Base
- before_create :generate_random_slug
- belongs_to :user
+  before_create :generate_random_slug
+  belongs_to :user
+  default_scope { order('created_at DESC') }
+
 
   def generate_random_slug
     self.slug = SecureRandom.urlsafe_base64
