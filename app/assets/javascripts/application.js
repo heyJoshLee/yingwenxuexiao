cd761342e34a68ef0e9d9009b96a9bd8603f6f70
Josh Lee <lee45276@gmail.com>
2017-08-16 02:27:06 +0800

    Fix lesson accordion problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require jquery.minicolors
 //= require_tree .
 
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -125,10 +126,11 @@ $(function() {
       heightStyle: "content",
       active: false
     });
+});
+
 
   $("#course_level_color").minicolors({theme: "bootstrap"});
 
-});
 
 
 
